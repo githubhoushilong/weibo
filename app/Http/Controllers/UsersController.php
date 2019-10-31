550ffc761e8eccbfedd3df0f0256526a9ccf6383
laravel <1255949790@qq.com>
2019-10-31 14:05:43 +0000

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,4 +14,15 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    //数据验证
+    public function store(Request $request){
+        //开始验证
+        $this->validate($request,[
+            'name' => 'required|max:50',//姓名必填，长度最大50
+            'email' => 'required|email|unique:users|max:255',//邮箱必填，email格式，唯一，最长255
+            'password' => 'required|confirmed|min:6'//密码必填，确认验证码，最短6
+        ]);
+        return;
+    }
 }

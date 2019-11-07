363c1851d7dca6a637090707c5d567981aa30404
laravel <1255949790@qq.com>
2019-11-07 05:57:23 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,4 +62,32 @@ class User extends Authenticatable
     public function feed(){
         return $this->statuses()->orderBy('created_at','desc');
     }
+
+    //用户粉丝一对多关系
+    public function followers(){
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    public function followings(){
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+    //用户关注
+    public function follow($user_ids){
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+    //取消关注
+    public function unfollow($user_ids){
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    //判断是否关注
+    public function isFollowing($user_id){
+        return $this->followings->contains($user_id);
+    }
 }
